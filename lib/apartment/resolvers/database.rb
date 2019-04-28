9c957b25bfea04f35d4525d5d828c01270fe3c00
Allen Eubank <adeubank@gmail.com>
2019-04-28 06:54:52 -0700

    Handle when default tenant might be passed in

--- lib/apartment/resolvers/database.rb
+++ lib/apartment/resolvers/database.rb
@@ -4,7 +4,7 @@ module Apartment
   module Resolvers
     class Database < Abstract
       def resolve(tenant)
-        return init_config if tenant.empty?
+        return init_config if tenant.empty? || init_config[:database] == tenant
         tenant, = tenant if tenant.is_a?(Array)
         tenant_names[tenant] || raise("Unable to resolve tenant #{tenant}")
       end
--- lib/apartment/version.rb
+++ lib/apartment/version.rb
@@ -1,3 +1,3 @@
 module Apartment
-  VERSION = "2.2.4"
+  VERSION = "2.2.5"
 end
